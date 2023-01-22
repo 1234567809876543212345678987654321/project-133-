e7383a23ee35f4d1d3709a7dfabc0f6dcd420661
1234567809876543212345678987654321 <115397531+1234567809876543212345678987654321@users.noreply.github.com>
2023-01-22 15:22:01 +0530

    Add files via upload

---
+++ light bulb .js
@@ -0,0 +1,45 @@
+Status = "";
+light_bulb_image = "";
+objects = [];
+
+function preload(){
+    light_bulb_image = loadImage("light bulb.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.Detect(light_bulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+            console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(light_bulb_image,0,0,640,350);
+    if(Status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("Status").innerHTML = "Status: Objects Detected";
+
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+
+    }
+}
